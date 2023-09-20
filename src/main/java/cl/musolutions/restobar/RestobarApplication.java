90d1f26023eaf2a47f5ff6a8a1788c8773d132db
Diego M. <diejamudelp@hotmail.com>
2023-09-19 23:53:28 -0300

    init

---
+++ src/main/java/cl/musolutions/restobar/RestobarApplication.java
@@ -0,0 +1,13 @@
+package cl.musolutions.restobar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestobarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestobarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/musolutions/restobar/RestobarApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.musolutions.restobar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestobarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
